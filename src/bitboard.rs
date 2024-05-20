4565aa2503b7cfa09344b7ca6aed38918cbdf51f
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-05-20 16:51:05 +0200

    finish basic bitboard methods, add piece and color enum

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -1,12 +1,32 @@
 /// A u64 where every bit represents every cell of the chess board
-/// For performance reasons it is inferred that all indices operating on this board are between 0-63
 pub struct BitBoard(pub u64);
 
 impl BitBoard {
     #[inline]
     /// Returns true if the bit at the given index is 1
     pub fn get_bit(&self, index: u8) -> bool {
-        (self.0 & (1 << index)) != 0
+        if index > 63 {
+            return false;
+        }
+        (self.0 & (1u64 << index)) != 0u64
+    }
+
+    #[inline]
+    /// Sets the bit at index to 1
+    pub fn set_bit(&mut self, index: u8) {
+        if index > 63 {
+            return;
+        }
+        self.0 |= 1u64 << index;
+    }
+
+    #[inline]
+    /// Sets the bit at index to 0
+    pub fn clear_bit(&mut self, index: u8) {
+        if index > 63 {
+            return;
+        }
+        self.0 &= !(1u64 << index);
     }
 }
 
@@ -21,5 +41,25 @@ mod tests {
         assert!(!b.get_bit(1));
         assert!(b.get_bit(2));
         assert!(!b.get_bit(3));
+    }
+
+    #[test]
+    fn test_set_bit() {
+        let mut b = BitBoard(0);
+        b.set_bit(0);
+        b.set_bit(1);
+        b.set_bit(2);
+        b.set_bit(3);
+        assert_eq!(b.0, 0b1111)
+    }
+
+    #[test]
+    fn test_clear_bit() {
+        let mut b = BitBoard(0b1111);
+        b.clear_bit(0);
+        b.clear_bit(1);
+        b.clear_bit(2);
+        b.clear_bit(3);
+        assert_eq!(b.0, 0)
     }
 }
---
+++ src/color.rs
@@ -0,0 +1,4 @@
+pub enum Color {
+    WHITE = 0,
+    BLACK = 1,
+}
