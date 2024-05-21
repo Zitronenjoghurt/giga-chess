7565888a4386add96084d019beb8beefe63c4f80
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-05-21 17:12:16 +0200

    benchmark different chess boards

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,14 +1,37 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use giga_chess::bitboard::BitBoard;
+use giga_chess::{
+    chess_boards::{chess_board12::ChessBoard12, chess_board8::ChessBoard8},
+    color::Color,
+    piece::Piece,
+};
 
-// It makes no sense to benchmark this, it's just for testing criterion, lol
-fn bitboard_get_bit() -> (bool, bool, bool, bool) {
-    let b = BitBoard(0b0101);
-    (b.get_bit(0), b.get_bit(1), b.get_bit(2), b.get_bit(3))
+fn chessboard_8() -> (Color, Color, Piece, Piece, (Piece, Color), (Piece, Color)) {
+    let cb = ChessBoard8::default();
+    (
+        cb.color_at_index(0),
+        cb.color_at_index(63),
+        cb.piece_at_index(0),
+        cb.piece_at_index(63),
+        cb.piece_and_color_at_index(0),
+        cb.piece_and_color_at_index(63),
+    )
+}
+
+fn chessboard_12() -> (Color, Color, Piece, Piece, (Piece, Color), (Piece, Color)) {
+    let cb = ChessBoard12::default();
+    (
+        cb.color_at_index(0),
+        cb.color_at_index(63),
+        cb.piece_at_index(0),
+        cb.piece_at_index(63),
+        cb.piece_and_color_at_index(0),
+        cb.piece_and_color_at_index(63),
+    )
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("bb get_bit", |b| b.iter(bitboard_get_bit));
+    c.bench_function("chessboard8", |b| b.iter(chessboard_8));
+    c.bench_function("chessboard12", |b| b.iter(chessboard_12));
 }
 
 criterion_group!(benches, criterion_benchmark);
