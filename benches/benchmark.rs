f4f90fe029515fc9ff09709f1e81dcc76ec5aead
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-05-22 12:41:16 +0200

    rework benchmarking to use a black box

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,32 +1,28 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-use giga_chess::{
-    chess_boards::{chess_board12::ChessBoard12, chess_board8::ChessBoard8},
-    color::Color,
-    piece::Piece,
-};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use giga_chess::chess_boards::{chess_board12::ChessBoard12, chess_board8::ChessBoard8};
 
-fn chessboard_8() -> (Color, Color, Piece, Piece, (Piece, Color), (Piece, Color)) {
+fn chessboard_8() {
     let cb = ChessBoard8::default();
-    (
+    black_box((
         cb.color_at_index(0),
         cb.color_at_index(63),
         cb.piece_at_index(0),
         cb.piece_at_index(63),
         cb.piece_and_color_at_index(0),
         cb.piece_and_color_at_index(63),
-    )
+    ));
 }
 
-fn chessboard_12() -> (Color, Color, Piece, Piece, (Piece, Color), (Piece, Color)) {
+fn chessboard_12() {
     let cb = ChessBoard12::default();
-    (
+    black_box((
         cb.color_at_index(0),
         cb.color_at_index(63),
         cb.piece_at_index(0),
         cb.piece_at_index(63),
         cb.piece_and_color_at_index(0),
         cb.piece_and_color_at_index(63),
-    )
+    ));
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
